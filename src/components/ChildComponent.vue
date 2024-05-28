2b58fd0ed4dd05dc0e20b8a9e4bc5ef7f8e9c3c0
MFatahurRahman <165864583+MFatahurRahman@users.noreply.github.com>
2024-05-28 16:52:02 +0700

    Add files via upload

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="child-container">
+    <h2>Child Component</h2>
+    <button @click="updateMessage">Ganti Pesan</button>
+    <button @click="emitData">Emit Data</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildComponent',
+  methods: {
+    updateMessage() {
+      this.$emit('message-change', 'Hello from Fatur');
+    },
+    emitData() {
+      const data = 'Some data from Fatur';
+      this.$emit('data-emitted', data);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.child-container {
+  background-color: #e6f7ff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  text-align: center;
+}
+
+h2 {
+  color: #333;
+}
+
+button {
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 10px 2px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+button:hover {
+  background-color: #e41010;
+}
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+      <h1>Parent Component</h1>
+      <p>Pesan: {{ message }}</p>
+      <ChildComponent @message-change="handleMessageChange" @data-emitted="displayData" />
+      <p>Data from Rahman: {{ childData }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import ChildComponent from './ChildComponent.vue';
+  
+  export default {
+    name: 'ParentComponent',
+    components: {
+      ChildComponent
+    },
+    data() {
+      return {
+        message: 'Hello from Rahman',
+        childData: ''
+      };
+    },
+    methods: {
+      handleMessageChange(newMessage) {
+        this.message = newMessage;
+      },
+      displayData(data) {
+        this.childData = data;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+ 
+.parent-container {
+  background-color: #f0f8ff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+h1 {
+  color: #333;
+}
+
+p {
+  color: #666;
+}
+
+  </style>
+  
